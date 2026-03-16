db266928569b4c06351b53b8d7f357673f25a77f
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2026-03-16 13:39:45 -0400

    Data Project Launch

---
+++ app/src/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 v-for="i in 10" :key="i">{{ text }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+const text = ref('')
+async function getdata() {
+    try{
+        const response = await fetch('/api/hello')
+        const data = await response.json()
+        text.value = data.message
+    }
+}
+onMounted(() => {
+    getdata()
+})
+</script>
+
+<style scoped></style>

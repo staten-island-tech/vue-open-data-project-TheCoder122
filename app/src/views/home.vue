91b0995d8fe61170b01000b2d53acc03c002119c
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2026-03-18 14:00:24 -0400

    Update Log: Data is live

--- app/src/views/home.vue
+++ app/src/views/home.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <h2 v-for="item in text" :key="item.cmplnt_num">
-      Complaint: {{ item.complaint_type }} <br />
-      Agency: {{ item.agency }} <br />
-      Descriptor: {{ item.descriptor }}
+    <div><h1>Deadly Sentencing</h1></div>
+    <h2 v-for="item in text" :key="item.inmate_id">
+      inmateid: {{ item.inmateid }} <br />
+      Custody_level: {{ item.custody_level }} <br />
+      race: {{ item.race }} <br />
+      age: {{ item.age }} <br />
+      top_charge: {{ item.top_charge }} <br />
     </h2>
   </div>
 </template>
@@ -12,7 +15,9 @@ import { ref, onMounted } from 'vue'
 const text = ref([])
 async function getdata() {
   try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/7479-ugqb.json?$limit=10')
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/7479-ugqb.json?$limit=99999999999999',
+    )
     const data = await response.json()
     text.value = data
     console.log('Fetched data:', data)

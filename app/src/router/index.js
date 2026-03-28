a7d02ba4ecbc4d9036a0b3548b38cfaafadec654
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2026-03-28 15:52:34 -0400

    Add search route to the router

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -6,10 +6,14 @@ const routes = [
     name: 'home',
     component: Home,
   },
+  {
+    path: '/search',
+    name: 'search',
+    component: Home,
+  },
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
 export default router
